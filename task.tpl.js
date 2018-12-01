10f6c0ce75f786e513340a272f50ad8d810710a0
Ian Savchenko (isao) <ian.savchenko@tobii.com>
2018-12-01 20:09:52 +0100

    add template for task

---
+++ task.tpl.js
@@ -0,0 +1,18 @@
+let _ = require('lodash');
+let fs = require('fs');
+
+// INPUT PROCESSING
+let data = fs.readFileSync('input.txt', 'utf8');
+let _input = data.split('\n').map(val => Number(val));
+
+// PART 1
+let part1 = 0;
+
+
+console.log(`Part 1: ${part1}`);
+
+// PART 2
+let part2 = 0;
+
+
+console.log(`Part 2: ${part2}`);
