41a5af0f0d0eb4f95ca7167c970f4667570a12c3
Ian Savchenko (isao) <ian.savchenko@tobii.com>
2018-12-01 20:09:36 +0100

    Solution to task 1

---
+++ 01/task.js
@@ -0,0 +1,18 @@
+let _ = require('lodash');
+let fs = require('fs');
+
+let data = fs.readFileSync('input.txt', 'utf8');
+let input = data.split('\n').map(val => Number(val));
+
+let part1 = _.sum(input);
+console.log(`Part 1: ${part1}`);
+
+let seen = {};
+let sum = 0;
+let i = 0;
+while (!(sum in seen)) {
+    seen[sum] = true;
+    sum += input[i++ % input.length];
+}
+
+console.log(`Part 2: ${sum}`);
