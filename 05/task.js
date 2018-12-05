b21d73ee593fc2b27ca4ea57d002a1da6dc477d6
Ian Savchenko (isao) <ian.savchenko@tobii.com>
2018-12-05 09:57:56 +0100

    optimize day 5 for speed - under 200ms from 35 seconds initially

--- 05/task.js
+++ 05/task.js
@@ -1,6 +1,5 @@
 let _ = require('lodash');
 let fs = require('fs');
-let util = require('../util');
 
 // INPUT PROCESSING
 let data = fs.readFileSync(__dirname + '/input.txt', 'utf8').trim().split('');
@@ -8,21 +7,48 @@ let data = fs.readFileSync(__dirname + '/input.txt', 'utf8').trim().split('');
 // PART 1
 let part1 = 0;
 
+let findPrev = function(removals, pos) {
+  if (pos === 0) {
+    return undefined;
+  }
+
+  if (!(pos - 1 in removals)) {
+    return pos - 1;
+  } else {
+    return findPrev(removals, removals[pos - 1]);
+  }
+};
+
 let remove = function(s) {
-  s = s.slice();
-  for (let i = 0; i < s.length - 1;) {
-    if (s[i] !== s[i + 1] && _.toLower(s[i]) === _.toLower(s[i + 1])) {
-      s.splice(i, 2);
-      i = i - 1;
+  let cur = 0;
+  let next = 1;
+  let removed = 0;
+  let removals = {};
+  while(next < s.length) {
+    if (s[cur] !== s[next] && _.toLower(s[cur]) === _.toLower(s[next])) {
+      removed += 2;   
+      removals[next] = cur; 
+
+      let newCur = findPrev(removals, cur);
+      if (newCur !== undefined) { 
+        // grow wide
+        cur = newCur;
+        next += 1;
+      } else {
+        // jump forward on next 2
+        cur = next + 1;
+        next += 2;
+      }
     } else {
-      i++;
+      cur = next;
+      next++;
     }
   }
 
-  return s;
+  return s.length - removed;
 };
 
-part1 = remove(data).length;
+part1 = remove(data);
 
 console.log(`Part 1: ${part1}`); // 10972
 
@@ -34,7 +60,7 @@ let z = 'z'.charCodeAt(0);
 for (let i = a; i <= z; i++) {
   let d = _.filter(data, c => !(c.charCodeAt(0) === i || _.toLower(c).charCodeAt(0) === i));
   d = remove(d);
-  part2 = Math.min(d.length, part2);
+  part2 = Math.min(d, part2);
 }
 
 console.log(`Part 2: ${part2}`); // 5278
