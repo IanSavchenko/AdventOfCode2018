e86c901d556fb833a89c7fd05e47f25594f0ac46
Ian Savchenko (isao) <ian.savchenko@tobii.com>
2018-12-12 10:04:41 +0100

    day 12 simplify

--- 12/task.js
+++ 12/task.js
@@ -6,89 +6,55 @@ let util = require('../util');
 let data = fs.readFileSync(__dirname + '/input.txt', 'utf8').trim();
 let lines = util.splitLines(data);
 
-let inp = [...lines[0].substr(15)].map(c => c === '#');
+let inp = [...lines[0].substr(15)];
 let rules = _.chain(lines).drop(2).map(line => {
-  let pattern = line.substr(0, 5).split('').map(c => c === '#');
-  let result = line[9] === '#';
+  let pattern = [...line.substr(0, 5)];
+  let result = line[9];
   return {
     pattern,
     result
   };
 }).value();
 
-let pots = _.map(inp, (s, n) => {
-  return {
-    s,
-    n
-  };
-});
-
+// PART 1 + 2
 let start = 0;
-let end = pots.length - 1;
-
 let it = [];
 let diffs = [];
 
-// PART 1
-let part1 = 0;
 for(let i = 0; i < 1000; i++) {
-  let newStart = _.findIndex(pots, {
-    s: true
+  while (!_.take(inp, 5).every(x => x === '.')) {
+    inp.splice(0, 0, '.');
+    start--;
+  }
+
+  while (!_.takeRight(inp, 5).every(x => x === '.')) {
+    inp.push('.');
+  }
+
+  inp = _.map(inp, function(_s, j) {
+    let sub = _.chain(inp).drop(j - 2).take(5).value();
+    let rule = _.find(rules, rule => {
+      return _.isEqual(rule.pattern, sub);
+    });
+
+    return rule ? rule.result : '.';
   });
 
-  while (start + 5 > newStart) {
-    pots.splice(0, 0, {
-      s: false,
-      n: --start
-    });
-    end++;
-  }
+  let cnt = _.chain(inp)
+    .map((s, n) => s === '#' ? n + start : 0)
+    .sum().value();
 
-  let newEnd = _.findLastIndex(pots, {
-    s: true
-  });
-
-  while (newEnd + 5 > end) {
-    pots.push({
-      s: false,
-      n: ++end + start
-    });
-  }
-
-  let newStates = [];
-  for(let j = 2; j < pots.length - 2; j++) {
-    let sub = _.chain(pots).drop(j - 2).take(5).value();
-    sub = sub.map(state => state.s);
-
-    let rule = _.find(rules, r => {
-      return _.isEqual(r.pattern, sub);
-    });
-
-    if (rule) {
-      newStates.push(rule.result);
-    } else {
-      newStates.push(false);
-    }
-  }
-
-  let k = 0;
-  for(let j = 2; j < pots.length - 2; j++) {
-    pots[j].s = newStates[k++];
-  }
-
-  it.push(_.chain(pots).filter(p => p.s).map('n').sum().value());
+  it.push(cnt);
   diffs.push(it[i] - it[i - 1]);
   if (_.every(_.takeRight(diffs, 100), d => d === diffs[i])) {
     break;
   }
 }
 
-part1 = it[19];
+let part1 = it[19];
 console.log(`Part 1: ${part1}`);
 console.assert(part1 === 1696);
 
-// PART 2
 let part2 = _.last(diffs) * (50*1000*1000*1000 - it.length) + _.last(it);
-
 console.log(`Part 2: ${part2}`);
 console.assert(part2 === 1799999999458);
