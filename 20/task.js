730afdd4bf6c6c0f3f75ef5b3d92fa6256a7c6f9
Ian Savchenko (isao) <ian.savchenko@tobii.com>
2018-12-20 08:09:56 +0100

    improve day 20

--- 20/task.js
+++ 20/task.js
@@ -5,6 +5,13 @@ let util = require('../util');
 // INPUT PROCESSING
 let data = fs.readFileSync(__dirname + '/input.txt', 'utf8').trim().split('');
 
+let dirs = {
+  N: {r: 1, c: 0, w: '-'},
+  S: {r: -1, c: 0, w: '-'},
+  E: {r: 0, c: 1, w: '|'},
+  W: {r: 0, c: -1, w: '|'},
+};
+
 // PART 1
 let parens = [];
 let map = util.arr2dInit(300, 300, '#');
@@ -12,25 +19,20 @@ let start = {r: Math.floor(map.length/2), c: Math.floor(map[0].length/2)};
 
 {
   let {r, c} = start;
+  map[start.r][start.c] = 'X';
   for(let ch of data) {
-    map[r][c] = '.';
+    if (ch in dirs) {
+      let d = dirs[ch];
+      r += d.r;
+      c += d.c;
+      map[r][c] = d.w;
+      r += d.r;
+      c += d.c;
+      map[r][c] = '.';
+      continue;
+    }
+
     switch(ch) {
-    case('N'):
-      map[--r][c] = '-';
-      r--;
-      break;
-    case('S'):
-      map[++r][c] = '-';
-      r++;
-      break;
-    case('W'):
-      map[r][--c] = '|';
-      c--;
-      break;
-    case('E'):
-      map[r][++c] = '|';
-      c++;
-      break;
     case('('):
       parens.push({r, c});
       break;
@@ -56,25 +58,14 @@ map[start.r][start.c] = 0;
 while (q.length) {
   const {r, c} = q.shift();
   let val = map[r][c];
-  if (map[r - 1][c] === '-' && map[r - 2][c] === '.') {
-    map[r - 2][c] = val + 1;
-    q.push({r: r - 2, c });
-  } 
 
-  if (map[r + 1][c] === '-' && map[r + 2][c] === '.') {
-    map[r + 2][c] = val + 1;
-    q.push({r: r + 2, c });
-  } 
-
-  if (map[r][c - 1] === '|' && map[r][c - 2] === '.') {
-    map[r][c - 2] = val + 1;
-    q.push({r, c: c - 2 });
-  } 
-  
-  if (map[r][c + 1] === '|' && map[r][c + 2] === '.') {
-    map[r][c + 2] = val + 1;
-    q.push({r, c: c + 2 });
-  } 
+  for(let d in dirs) {
+    d = dirs[d];
+    if (map[r + d.r][c + d.c] === d.w && map[r + 2*d.r][c + 2*d.c] === '.') {
+      map[r + 2*d.r][c + 2*d.c] = val + 1;
+      q.push({r: r + 2*d.r, c: c + 2*d.c });
+    }
+  }
 
   part1 = Math.max(val, part1);
   if (val >= 1000) {
